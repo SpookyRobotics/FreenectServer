c6b045d94c011d45fbe568daf7e8d37989b1e9fd
jsjrobotics <jsjrobotics@gmail.com>
2016-06-04 15:30:57 -0400

    InputPanel stich and split working

--- src/main/java/com/spookybox/frameConsumers/InputPanel.java
+++ src/main/java/com/spookybox/frameConsumers/InputPanel.java
@@ -11,11 +11,17 @@ public class InputPanel {
     public final int[] image;
     public final int panelWidth;
     public final int panelHeight;
+    private final int startX;
+    private final int startY;
 
     public InputPanel(
+            int startX,
+            int startY,
             int panelWidth,
             int panelHeight,
             int[] panelImage) {
+        this.startX = startX;
+        this.startY = startY;
         this.image = panelImage;
         this.panelWidth = panelWidth;
         this.panelHeight = panelHeight;
@@ -24,18 +30,13 @@ public class InputPanel {
     static BufferedImage stichPanelsToImage(final InputPanelImage inputPanelImage) {
         List<InputPanel> panels = inputPanelImage.panels;
         int imageWidth = panels.get(0).panelWidth * inputPanelImage.panelsInRow;
-        int imageHeight = panels.get(0).panelHeight * panels.size() / inputPanelImage.panelsInRow;
+        int imageHeight = panels.get(0).panelHeight * inputPanelImage.rows;
 
-        int[] imageBuffer = new int[imageHeight * imageWidth];
-        int imageBufferIndex = 0;
-        for(InputPanel p : panels) {
-            for (int index = 0; index < p.image.length; index++) {
-                imageBuffer[imageBufferIndex] = p.image[index];
-                imageBufferIndex += 1;
-            }
-        }
         BufferedImage image = new BufferedImage(imageWidth, imageHeight, TYPE_INT_ARGB);
-        image.setRGB(0, 0, imageWidth, imageHeight, imageBuffer, 0, imageWidth);
+        for(InputPanel p : inputPanelImage.panels){
+            BufferedImage subImage = image.getSubimage(p.startX, p.startY, p.panelWidth, p.panelHeight);
+            subImage.setRGB(0, 0, p.panelWidth, p.panelHeight, p.image, 0, p.panelWidth);
+        }
         return image;
     }
 
@@ -47,12 +48,24 @@ public class InputPanel {
         ArrayList<InputPanel> result = new ArrayList<>();
         int panelWidth = image.getWidth() / panelsPerRow;
         int panelHeight = image.getHeight() / numberOfRows;
-        int[] imageData = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
-        for(int index = 0; index < imageData.length; index += panelHeight){
+        for(int panelIndex = 0; panelIndex < panelsPerRow * numberOfRows; panelIndex++){
+            int startX = (panelIndex % panelsPerRow) * panelWidth;
+            int startY = (panelIndex / panelsPerRow) * panelHeight;
+            int readWidth = panelWidth;
+            int readHeight = panelHeight ;
+            if(startX + readWidth > image.getWidth()){
+                readWidth = image.getWidth() - startX;
+            }
+            if(startY + readHeight > image.getHeight()){
+                readHeight = image.getHeight() - startY;
+            }
+            BufferedImage subImage = image.getSubimage(startX, startY, readWidth, readHeight);
             result.add(new InputPanel(
-                    panelWidth,
-                    panelHeight,
-                    Arrays.copyOfRange(imageData, index, index+panelHeight)
+                    startX,
+                    startY,
+                    readWidth,
+                    readHeight,
+                    subImage.getRGB(0,0,subImage.getWidth(), subImage.getHeight(), null, 0, subImage.getWidth())
             ));
         }
         return new InputPanelImage(result, panelsPerRow);
--- src/main/java/com/spookybox/frameConsumers/InputPanelImage.java
+++ src/main/java/com/spookybox/frameConsumers/InputPanelImage.java
@@ -6,9 +6,12 @@ import java.util.List;
 public class InputPanelImage {
     public final List<InputPanel> panels;
     public final int panelsInRow;
+    public final int rows;
 
     public InputPanelImage(List<InputPanel> panels, int panelsInRow) {
         this.panels = panels;
         this.panelsInRow = panelsInRow;
+        this.rows = panels.size() / panelsInRow;
+
     }
 }
